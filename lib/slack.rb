904bd3fbe0e072ba30050c9a6e2004bb8ecce310
Mina Shin <minamishin@hotmail.com>
2019-03-18 16:58:52 -0700

    testing slack

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,31 @@
-#!/usr/bin/env ruby
+# #!/usr/bin/env ruby
 
-def main
-  puts "Welcome to the Ada Slack CLI!"
+# def main
+#   puts "Welcome to the Ada Slack CLI!"
 
-  # TODO project
+#   # TODO project
 
-  puts "Thank you for using the Ada Slack CLI"
+#   puts "Thank you for using the Ada Slack CLI"
+# end
+
+# main if __FILE__ == $PROGRAM_NAME
+
+require "dotenv"
+require "httparty"
+
+# Tell dotenv to look for the .env file
+Dotenv.load
+
+url = "https://slack.com/api/channels.list"
+
+query = {
+  token: ENV["KEY"],
+}
+
+response = HTTParty.get(url, query: query)
+
+if response.message != 200
+  raise ArgumentError, "Request is unsuccessful"
+else
+  puts "#{response}"
 end
-
-main if __FILE__ == $PROGRAM_NAME
