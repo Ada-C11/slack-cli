ef3bba4c8779bc40c63b362675443765b4327251
Carla Bosco <carlabosco@gmail.com>
2019-03-19 16:08:21 -0700

    Added SlackApiError class

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -4,8 +4,11 @@ require "dotenv"
 Dotenv.load
 
 module SlackCLI
+
+  class SlackApiError < Exception; end
+
   class Workspace
-    attr_reader :users, :channels
+    attr_reader :users, :channels, :selected
 
     def initialize(users:, channels:)
       @users = users
