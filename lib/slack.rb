8bbb51bb7d02a0965255473012dcd4abe0568664
Shubha Rajan <shubharajan94@gmail.com>
2019-03-19 14:46:56 -0700

    added driver code to main.rb and test for User#display_details

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require_relative "channel"
+
 require "httparty"
 require "dotenv"
 require "awesome_print"
@@ -9,7 +10,21 @@ Dotenv.load
 
 def main
   puts "Welcome to the Ada Slack CLI!"
-
+  puts "What would you like to do?"
+  puts "Options: \nlist users \nlist channels \nquit"
+  input == gets.chomp.downcase
+  until (input == "quit")
+    case "input"
+    when "list users"
+      puts SlackCLI::Workspace.display_users
+    when "list channels"
+      puts SlackCLI::Workspace.display_channels
+    when "quit"
+      break
+    else
+      puts "That wasn't one of the options!"
+    end
+  end
   # TODO project
 
   puts "Thank you for using the Ada Slack CLI"
