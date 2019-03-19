c6fed721a2267ef3706056a954be2c62f6d71222
sjscotton <sj.scotton@gmail.com>
2019-03-19 16:16:53 -0700

    Added main loop in main.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,26 +1,53 @@
 #!/usr/bin/env ruby
-
+require "table_print"
 require "HTTParty"
 require "dotenv"
 Dotenv.load
 
-url = "https://slack.com/api/channels.list?"
-token = ENV["TOKEN"]
+require_relative "../lib/recipient.rb"
+require_relative "../lib/user.rb"
+require_relative "../lib/channel.rb"
+require_relative "../lib/workspace.rb"
 
-query_parameters = {
-  token: token,
-}
+def get_user_input(options)
+  input = gets.chomp.to_s.strip
+  unless options.include?(input)
+    puts "Please choose option from list"
+    input = gets.chomp.to_s.strip
+  end
+  return input
+end
 
-response = HTTParty.get(url, query: query_parameters)
+def list_options
+  puts "Please choose from these options"
+  puts "Enter 'list users' to view all users"
+  puts "Enter 'list channels' to view all channels"
+  puts "Enter 'quit' to quit"
+end
 
-puts response
+def create_table
+end
 
-# def main
-#   puts "Welcome to the Ada Slack CLI!"
+def main
+  puts "Welcome to the Ada Slack CLI!"
+  workspace = SlackBot::Workspace.new
 
-#   # TODO project
+  while true
+    list_options
+    options = ["list users", "list channels", "quit"]
+    choice = get_user_input(options)
+    case choice
+    when "list users"
+      puts "#{workspace.users}"
+    when "list channels"
+      puts "#{workspace.channels}"
+    when "quit"
+      puts "Goodbye!"
+      exit
+    end
+  end
 
-#   puts "Thank you for using the Ada Slack CLI"
-# end
+  puts "Thank you for using the Ada Slack CLI"
+end
 
-# main if __FILE__ == $PROGRAM_NAME
+main if __FILE__ == $PROGRAM_NAME
