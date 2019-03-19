b7f136a7457b2045bf1ad56240d7364a97b01d26
Tatiana Quintana <tatsqui@gmail.com>
2019-03-18 17:17:00 -0700

    added .env and did verification

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,25 @@
 #!/usr/bin/env ruby
+require "dotenv"
+require "httparty"
+require "pry"
+require "awesome_print"
+Dotenv.load
 
 def main
   puts "Welcome to the Ada Slack CLI!"
 
-  # TODO project
+  url = "https://slack.com/api/channels.list"
 
+  query_params = { token: ENV["API_KEY"] }
+
+  response = HTTParty.get(url, query: query_params)
+
+  response["channels"].each do |channel|
+    puts channel["name"]
+  end
   puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
+
+# response["channels"][0]["name"]
