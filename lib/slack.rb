37abe63aa3faf5f2b83add8952dedc77240dbdcc
amyesh <amyesh08@gmail.com>
2019-03-19 13:48:49 -0700

    completed verification

--- lib/slack.rb
+++ lib/slack.rb
@@ -12,10 +12,11 @@ query = {
   token: ENV["SLACK_API_TOKEN"],
 }
 channel_info = HTTParty.get(BASE_URL, query: query)
-channel_list = channel_info.map do |channel|
+channel_list = channel_info["channels"]
+channel_names = channel_list.map do |channel|
   channel["name"]
 end
-ap channel_list
+ap channel_names
 #puts "Thank you for using the Ada Slack CLI"
 #end
 
