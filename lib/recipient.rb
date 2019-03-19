deb36349750e79c1721429503041a0ffbcab7c87
Erica Norris <norrise120@gmail.com>
2019-03-19 14:04:36 -0700

    Wrote constructor for Recepicient, fixed recipient spec

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,5 +1,10 @@
 module SlackCLI
   class Recipient
-    #code
+    attr_reader :slack_id, :name
+
+    def initialize(slack_id, name)
+      @slack_id = slack_id
+      @name = name
+    end
   end
 end
--- specs/recipient_spec.rb
+++ specs/recipient_spec.rb
@@ -1,16 +1,20 @@
 require_relative "test_helper"
 
 describe SlackCLI::Recipient do
-  describe 'constructor' do
-    it 'takes and saves a Slack id and name' do
-      VCR.use_cassette("recipient") do
-        slack_id = "CH2SKTDBN"
-        name = "random"
-        record = SlackCLI::Recipient.new(slack_id, name)
-        expect(record.slack_id).must_equal slack_id
-        expect(record.name).must_equal name
-        expect(record).must_be_kind_of SlackCLI::Recipient
-      end
+  describe "constructor" do
+    before do
+      @slack_id = "CH2SKTDBN"
+      @name = "random"
+      @record = SlackCLI::Recipient.new(@slack_id, @name)
+    end
+
+    it "is an instance of Recipient" do
+      expect(@record).must_be_kind_of SlackCLI::Recipient
+    end
+
+    it "takes and saves a Slack id and name" do
+      expect(@record.slack_id).must_equal @slack_id
+      expect(@record.name).must_equal @name
     end
   end
 end
