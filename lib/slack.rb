dbbcbcda44388a769de608299d0a6ad0b574efc0
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-19 13:43:10 -0700

    testing Slack API Authorization

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
+require "dotenv"
+require "httparty"
+
+Dotenv.load
+
+URL = "https://slack.com/api/channels.list"
+
+response = HTTParty.get(URL, query: {token: ENV["KEY"]})
+
+(0..response["channels"].length - 1).each do |index|
+  puts response["channels"][index]["name"]
+end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
