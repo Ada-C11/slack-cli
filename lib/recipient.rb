531e18ffbc31f19dac32f98dd38910bc130bb9e7
Tatiana Quintana <tatsqui@gmail.com>
2019-03-19 15:53:19 -0700

    wrote tests for .send_message and tests are green

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -4,7 +4,10 @@ require "pry"
 require "dotenv"
 Dotenv.load
 
+class SlackError < StandardError; end
+
 class Recipient
+
   # constants
   MSG_URL = "https://slack.com/api/chat.postMessage"
   USER_URL = "https://slack.com/api/users.list" # Move to User
@@ -18,24 +21,17 @@ class Recipient
   end
 
   def self.send_message(name:, message:)
-    # using HTTParty.post(MSG_URL, message, slack_id)
     query_params = {
       token: ENV["SLACK_TOKEN"],
       channel: name,
       text: message,
     }
-    # send a message to a recipient that matches provided slack_id
     response = HTTParty.post(MSG_URL, query: query_params)
-    # return response message/code
-    # if response["ok"]
-    #   return response
-    # else
-    #    "#{response}: Message failed :("
-    # end
-    return response
+
+    if response["ok"] != true
+      raise SlackError, "#{response["error"]}"
+    else
+      return response
+    end
   end
 end
-
-# test = Recipient.send_message("everyone", "this is a string!")
-
-# puts test.code
--- specs/recipient_spec.rb
+++ specs/recipient_spec.rb
@@ -5,18 +5,24 @@ describe "Recipient" do
   end
 
   describe "send message" do
-
     it "can post a message on the everyone slack channel" do
       VCR.use_cassette("message_post") do
         name = "everyone"
-        text = "Hey there%21"
+        text = "hello world"
         pigeon = Recipient.send_message(name: name, message: text)
 
         expect(pigeon["ok"]).must_equal true
       end
+    end
 
-      it "raises an exception for bad request" do 
-        
+    it "raises a SlackError with bad parameters" do
+      VCR.use_cassette("message_post") do
+        name = ""
+        text = "this should not send"
+
+        expect {
+          Recipient.send_message(name: name, message: text)
+        }.must_raise SlackError
       end
     end
   end
