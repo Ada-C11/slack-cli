ba5104dae6bf3cb2cb3117c783f5edc77be79bc8
Angela <angela.ohh@gmail.com>
2019-03-19 16:30:34 -0700

    added driver code to slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -9,29 +9,32 @@ def main
   puts "Please choose one of the the following number options"
   puts "1. List Users"
   puts "2. List Channels"
-  puts "3. Quit"
+  puts "3. Select User"
+  puts "4. Select Channel"
+  puts "5. Quit"
 
   selection = gets.chomp
 
   loop do
-    if selection == "1"
+    case selection
+    when "1"
       puts "Here is the list of users:"
       puts User.list
-    elsif selection == "2"
+    when "2"
       puts "Here is the list of channels: "
       puts Channel.list
-    elsif selection == "3"
+    when "3"
+      chose_user = gets.chomp
+      workspace = Workspace.new
+      puts workspace.show_details_user(chose_user)
+    when "4"
+      chose_channel = gets.chomp
+      workspace = Workspace.new
+      puts workspace.show_details_channel(chose_channel)
+    when "5"
       break
-    else
-      puts "Please choose one of the the following number options"
-      puts "1. List Users"
-      puts "2. List Channels"
-      puts "3. Quit"
-
-      selection = gets.chomp
     end
   end
-  # TODO project
 
   puts "Thank you for using the Ada Slack CLI"
 end
