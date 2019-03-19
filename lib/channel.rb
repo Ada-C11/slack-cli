01193f9dc51826a3896e0a1d3f20aaceb9702f51
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-19 15:11:09 -0700

    Moved Slack#list_channel to Channel#self.list

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,21 +1,27 @@
 module SlackAPI
-
-    class Channel
+  class Channel
         
-        attr_reader :topic, :member_count
+  attr_reader :topic, :member_count
 
-        def initialize(topic:, member_count:)
-            super(slack_id:, name:)
-            @topic = topic
-            @member_count = member_count
-        end
-
-        def details
-        end
-
-        def self.list
-        end 
-
+    def initialize(topic:, member_count:)
+      super(slack_id:, name:)
+      @topic = topic
+      @member_count = member_count
     end
 
+    def details
+    end
+
+    def self.list
+      channel_names = []
+      query_parameters = {
+        token: ENV['TOKEN'],
+      }
+      channels = HTTParty.get(BASE_URL << CHANNELS_LIST_PATH, query: query_parameters)['channels']
+      channel_names = channels.map do |channel|
+      channel['name']
+      end
+      return channel_names
+    end
+  end
 end
--- lib/slack.rb
+++ lib/slack.rb
@@ -10,18 +10,6 @@ CHAT_POST_MESSAGE_PATH = '/chat.postMessage'
 
 token = ENV['TOKEN']
 
-def list_channels
-  channel_names = []
-  query_parameters = {
-    token: ENV['TOKEN'],
-  }
-  channels = HTTParty.get(BASE_URL << CHANNELS_LIST_PATH, query: query_parameters)['channels']
-  channel_names = channels.map do |channel|
-    channel['name']
-  end
-  return channel_names
-end
-
 def main
   puts "Welcome to the Ada Slack CLI!"
   puts "Channel names: #{list_channels}"
