c1721fb1a188396d56ad18dbcbcb86701b5ccd9e
Jessica <jessica.homet@gmail.com>
2019-03-18 22:20:42 -0700

    Verify API connection

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,9 +1,26 @@
 #!/usr/bin/env ruby
+require "dotenv"
+require "httparty"
+
+Dotenv.load
 
 def main
   puts "Welcome to the Ada Slack CLI!"
 
+  url = "https://slack.com/api/channels.list"
+
+  query_parameters = {
+    token: ENV["SLACK_API_TOKEN"],
+  }
+
+  response = HTTParty.get(url, query: query_parameters)
   # TODO project
+
+  if response.code == 200
+    puts response
+  else
+    puts "#{response.code} #{response.message}"
+  end
 
   puts "Thank you for using the Ada Slack CLI"
 end
