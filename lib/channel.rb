7e66ef94c4878e5bf00299aeaa9d38d86b93efbc
Erica Norris <norrise120@gmail.com>
2019-03-19 15:53:46 -0700

    Created self.list for Channel class

--- lib/channel.rb
+++ lib/channel.rb
@@ -10,5 +10,23 @@ module SlackCLI
       @topic = topic
       @member_count = member_count
     end
+
+    def self.list
+      url = "https://slack.com/api/channels.list"
+      data = {
+        "token": ENV["SLACK_API_TOKEN"],
+      }
+      response = self.get(url, data)
+      channels = []
+      response["channels"].each do |channel|
+        slack_id = channel["id"]
+        name = channel["name"]
+        topic = channel["topic"]["value"]
+        member_count = channel["num_members"]
+        channel = self.new(slack_id: slack_id, name: name, topic: topic, member_count: member_count)
+        channels << channel
+      end
+      return channels
+    end
   end
 end
