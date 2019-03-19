f43603495835e502d9025c927c13bc3e0c0f3f03
Kelly Downes <kedownes@gmail.com>
2019-03-19 16:20:52 -0700

    Add workspace constructor tests

--- specs/workspace_spec.rb
+++ specs/workspace_spec.rb
@@ -1,1 +1,23 @@
 require_relative "test_helper"
+
+describe SlackCLI::Workspace do
+  describe "constructor" do
+    before do
+      VCR.use_cassette("workspace") do
+        @workspace = Workspace.new
+      end
+    end
+
+    it "is an instance of Channel" do
+      expect(@workspace).must_be_kind_of SlackCLI::Workspace
+    end
+
+    it "has a collection of users, collection of channels, and a selected recipient is nil" do
+      expect(@workspace.users).must_be_kind_of Array
+      expect(@workspace.users.first).must_be_kind_of SlackCLI::User
+      expect(@workspace.channels).must_be_kind_of Array
+      expect(@workspace.channels.first).must_be_kind_of SlackCLI::Channel
+      expect(@workspace.selected).must_be_nil
+    end
+  end
+end
