e0c01e4203a4d3e63bad12db66b2c7b2c6a3369a
Tatiana Quintana <tatsqui@gmail.com>
2019-03-19 13:46:13 -0700

    configuring test_helper.rb

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -1,11 +1,17 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
-require 'vcr'
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/skip_dsl"
+require "vcr"
+
+require_relative "lib/slack"
+require_relative "lib/channel"
+require_relative "lib/user"
+require_relative "lib/workspace"
+require_relative "lib/recipient"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
