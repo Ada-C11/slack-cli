46d21043e7fd66303a9cad4b3310e115f39a95b3
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-19 11:35:06 -0700

    Added #list_channels, setup Slack auth

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,10 +1,26 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
+Dotenv.load
+
+BASE_URL = 'https://slack.com/api/channels.list'
+token = ENV['TOKEN']
+
+def list_channels
+  channel_names = []
+  query_parameters = {
+    token: ENV['TOKEN'],
+  }
+  channels = HTTParty.get(BASE_URL, query: query_parameters)['channels']
+  channel_names = channels.map do |channel|
+    channel['name']
+  end
+  return channel_names
+end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
-
-  # TODO project
-
+  puts "Channel names: #{list_channels}"
   puts "Thank you for using the Ada Slack CLI"
 end
 
