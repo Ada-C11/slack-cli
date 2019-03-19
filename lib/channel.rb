c8a83ffc88220d32c786509f4b5cbfa841547f4b
Angela <angela.ohh@gmail.com>
2019-03-19 11:31:42 -0700

    set up Channel class

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,1 +1,20 @@
+require "httparty"
+require "env"
+Dotenv.load
 
+class Channel
+  attr_reader :name, :topic, :member_count, :id
+
+  def initialize
+    @name = name
+    @topic = topic
+    @member_count = member_count #some code that counts users.
+    @id = id
+  end
+
+  def details(name, topic, member_count, id)
+  end
+
+  def self.list
+  end
+end
