fde573e5574c96edbcc8cec44e47038aa88eb495
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-19 15:08:33 -0700

    removed ap ENV token from method, just put it in to test if it works.

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,14 +1,14 @@
 require "HTTParty"
+require "dotenv"
 require "awesome_print"
 
 module Slack
   class Channel
+    CHANNEL_URL = "https://slack.com/api/channels.list"
+
     class SlackError < StandardError; end
 
-    CHANNEL_URL = "https://slack.com/api/channels.list"
-    query_parameters = {
-      token: ENV["SLACK_API_TOKEN"],
-    }
+    Dotenv.load
 
     attr_reader :channel_id, :channel_name, :topic, :member_count
     attr_accessor :channel_name, :topic
@@ -20,11 +20,22 @@ module Slack
       @member_count = member_count
     end # initialize
 
-    # def self.list
-    #   response = HTTParty(CHANNEL_URL, query: query_parameters)
-    #   ap ENV["SLACK_API_TOKEN"]
+    def self.list
+      query_parameters = {
+        token: ENV["SLACK_API_TOKEN"],
+      }
+      response = HTTParty.get(CHANNEL_URL, query: query_parameters)
+      ENV["SLACK_API_TOKEN"]
 
-    #   if(response.code == 200)
-    # end # self.list
+      if (response.code == 200)
+        response["channels"].map do |channel|
+          channel["name"]
+        end # each
+      else
+        puts "Error #{response.code} : #{response["message"]}"
+      end # else
+    end # self.list
+
+    ap self.list
   end # class
 end # module
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -3,24 +3,24 @@ require "dotenv"
 require "awesome_print"
 require "pry"
 
-Dotenv.load
+# Dotenv.load
 
-BASE_URL = "https://slack.com/api/users.list"
-query_parameters = {
-  token: ENV["SLACK_API_TOKEN"],
-}
+# BASE_URL = "https://slack.com/api/users.list"
+# query_parameters = {
+#   token: ENV["SLACK_API_TOKEN"],
+# }
 
-response = HTTParty.get(BASE_URL, query: query_parameters)
-ap ENV["SLACK_API_TOKEN"]
+# response = HTTParty.get(BASE_URL, query: query_parameters)
+# ap ENV["SLACK_API_TOKEN"]
 
-ap response
-if (response.code == 200)
-  response["members"].each do |user|
-    puts user["name"]
-  end
-else
-  puts "Error #{response.code} : #{response["message"]}"
-end
+# ap response
+# if (response.code == 200)
+#   response["members"].each do |user|
+#     puts user["name"]
+#   end
+# else
+#   puts "Error #{response.code} : #{response["message"]}"
+# end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
