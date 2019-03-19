2124ce84cce836b732ee90db11bcf634d89865f9
Jessica <jessica.homet@gmail.com>
2019-03-19 15:43:02 -0700

    Set up frame work and created self.list (unfinished)

---
+++ lib/user.rb
@@ -0,0 +1,43 @@
+require_relative "recipient"
+
+module Slack
+  class User < Recipient
+    attr_reader :real_name, :status_text, :status_emoji
+
+    def initialize(slack_id, name, real_name, status_text, status_emoji)
+      super(slack_id, name)
+      @real_name = real_name
+      @status_text = status_text
+      @status_emoji = status_emoji
+    end
+
+    #temp
+    def self.list
+      list = []
+      Dotenv.load
+
+      url = "https://slack.com/api/users.list"
+
+      query_parameters = {
+        token: ENV["SLACK_API_TOKEN"],
+      }
+
+      response = HTTParty.get(url, query: query_parameters)
+
+      if response.code == 200
+        response.parsed_response["members"].each do |member|
+          slack_id = member["id"]
+          name = member["name"]
+          real_name = member["real_name"]
+          status_text = member["profile"]["status_text"]
+          status_emoji = member["status_emoji"]
+
+          list << Slack::User.new(slack_id, name, real_name, status_text, status_emoji)
+        end
+      else
+        ap "#{response.code} #{response.message}"
+      end
+      return list
+    end
+  end
+end
---
+++ specs/user_spec.rb
@@ -0,0 +1,21 @@
+require_relative "test_helper"
+
+describe "User" do
+  describe "Initialization" do
+    let(:slack_id) { 20 }
+    let(:username) { "soph" }
+    let(:real_name) { "Sopheary" }
+    let(:status_text) { "Good to go" }
+    let(:status_emoji) { ":)" }
+
+    it "can be instantiated" do
+      user = Slack::User.new(slack_id, username, real_name, status_text, status_emoji)
+      expect(user).must_be_instance_of Slack::User
+      expect(user.slack_id).must_equal 20
+      expect(user.name).must_equal "soph"
+      expect(user.real_name).must_equal "Sopheary"
+      expect(user.status_text).must_equal "Good to go"
+      expect(user.status_emoji).must_equal ":)"
+    end
+  end
+end
