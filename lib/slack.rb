c9c72427c105b0c6f7a83df6e003795a1d988b3e
Angela <angela.ohh@gmail.com>
2019-03-19 14:38:26 -0700

    created main method in Slack.rb file

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,19 +1,40 @@
 #!/usr/bin/env rub
 require "dotenv"
+
 def main
-BASE_URL = 'https://slack.com/api/channels.list'
- SLACK_TOKEN = ENV["SLACK_TOKEN"]
-
-
   puts "Welcome to the Ada Slack CLI!"
- 
+  puts "We currently have #{User.list.length} members and #{Channel.list.length} channels."
 
+  puts ""
+  puts "Please choose one of the the following number options"
+  puts "1. List Users"
+  puts "2. List Channels"
+  puts "3. Quit"
 
+  selection = gets.chomp
+
+  loop do
+    if selection == "1"
+      puts "Here is the list of users:"
+      puts User.list
+    elsif selection == "2"
+      puts "Here is the list of channels: "
+      puts Channel.list
+    elsif selection == "3"
+      break
+    else
+      puts "Please choose one of the the following number options"
+      puts "1. List Users"
+      puts "2. List Channels"
+      puts "3. Quit"
+
+      selection = gets.chomp
+    end
+  end
   # TODO project
 
   puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
-
-
+# main
