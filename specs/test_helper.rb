041a674e8b347dbd5ed5d1ae67074bd559d7458f
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-19 14:08:23 -0700

    api token variable changed

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -21,8 +21,9 @@ VCR.configure do |config|
     :record => :new_episodes,    # record new data when we don't have it yet
     :match_requests_on => [:method, :uri, :body], # The http method, URI and body of a request all need to match
   }
+
   # Don't leave our Slack token lying around in a cassette file.
-  config.filter_sensitive_data("<LOCATIONIQ_TOKEN>") do
-    ENV["LOCATIONIQ_TOKEN"]
+  config.filter_sensitive_data("<SLACKAPI_TOKEN>") do
+    ENV["SLACK_API_TOKEN"]
   end
 end
