91a050ad664e198c50a10a4ad534a76a6bf9bf46
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-19 15:01:30 -0700

    Fixed another merge conflict

--- lib/slack.rb
+++ lib/slack.rb
@@ -3,24 +3,12 @@ require 'dotenv'
 require 'httparty'
 Dotenv.load
 
-<<<<<<< HEAD
 BASE_URL = 'https://slack.com/api'
 CHANNELS_LIST_PATH = '/channels.list'
 USERS_LIST_PATH = '/users.list'
 CHAT_POST_MESSAGE_PATH = '/chat.postMessage'
 
 token = ENV['TOKEN']
-=======
-class SlackAPI
-
-
-  class SlackAPIError < StandardError; end
-
-end
-
-def main
-  puts "Welcome to the Ada Slack CLI!"
->>>>>>> 5348b3269c38949f34dd238e6b3d833644bcdb73
 
 def list_channels
   channel_names = []
