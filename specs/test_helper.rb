a2888cb14ae89e0f0061a91b272d1a61b1900fd3
Jessica <jessica.homet@gmail.com>
2019-03-19 10:22:44 -0700

    Change single quotes to double quotes (autoformat)

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -1,11 +1,11 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
-require 'vcr'
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/skip_dsl"
+require "vcr"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
