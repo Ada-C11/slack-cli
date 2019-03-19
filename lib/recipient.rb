b6f7258c58982409b0b39955ef4382a281ec4ac6
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-19 14:46:20 -0700

    added get method and test for connectivity

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -4,8 +4,14 @@ require "httparty"
 class Recipient
   attr_reader :slack_id, :name
 
-  def initialize(slack_id: nil, name: nil)
+  def initialize(slack_id:, name:)
     @slack_id = slack_id
+    raise ArgumentError if !name.is_a? String
+
     @name = name
+  end
+
+  def self.get(url, params)
+    response = HTTParty.get(url, query: params)
   end
 end
--- specs/recipient_spec.rb
+++ specs/recipient_spec.rb
@@ -3,9 +3,26 @@ require_relative "test_helper"
 describe "recipient class" do
   describe "initialize" do
     it "creates an instance of Recipient" do
+      #check slack_if format
       slack_id = 1
       name = "Maria"
       expect(Recipient.new(slack_id: slack_id, name: name)).must_be_kind_of Recipient
+    end
+
+    it "raises an argument error if name is not a string" do
+      expect { Recipient.new(slack_id: 1, name: 21) }.must_raise ArgumentError
+    end
+  end
+
+  describe "can connect to API" do
+    it "can connect" do
+      VCR.use_cassette("find channels") do
+        BASE_URL = "https://slack.com/api/channels.list"
+        params = {token: ENV["SLACK_API_TOKEN"]}
+        response = Recipient.get(BASE_URL, params)
+        expect(response["channels"]).wont_be_nil
+        expect(response["channels"].map { |channel| channel["name"] }).must_equal ["general", "api-project", "random"]
+      end
     end
   end
 end
