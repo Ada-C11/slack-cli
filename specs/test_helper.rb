3bfc6004461e7f104060cdd5115a53fe0c87dacd
Jessica <jessica.homet@gmail.com>
2019-03-19 15:45:29 -0700

    Add require_relatives and started messing with VCR (not finished)

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -5,11 +5,25 @@ require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
 require "minitest/skip_dsl"
-require "vcr"
+# require "vcr"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-VCR.configure do |config|
-  config.cassette_library_dir = "specs/cassettes"
-  config.hook_into :webmock
-end
+require_relative "../lib/channel"
+require_relative "../lib/user"
+require_relative "../lib/recipient"
+require_relative "../lib/workspace"
+# require_relative "../lib/slack" # Do we need this? Don't think so.
+
+# VCR.configure do |config|
+#   config.cassette_library_dir = "specs/cassettes" # folder where casettes will be
+#   config.hook_into :webmock # tie into this other rool called webmock
+#   config.default_cassette_options = {
+#     :record => :new_episodes, # record new data when we don't have it yet
+#     :match_requests_on => [:method, :uri, :body],
+#   }
+#   # Don't leave our token lying around in cassette file.
+#   config.filter_sensitive_data("<SLACK_API_TOKEN>") do
+#     ENV["SLACK_API_TOKEN"]
+#   end
+# end
