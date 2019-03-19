97d7f3dfbe6b1cbba8bb01fa2aa8ceb3154b0c8c
Kelly Downes <kedownes@gmail.com>
2019-03-19 11:28:13 -0700

    Setup environment

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -6,10 +6,20 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
+require "webmock/minitest"
+require "dotenv"
+Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
   config.cassette_library_dir = "specs/cassettes"
   config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:method, :uri, :body], 
+  }
+  config.filter_sensitive_data("<SLACK_API_TOKEN>") do
+    ENV["SLACK_API_TOKEN"]
+  end
 end
