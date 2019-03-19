ff7f7a256bb1d196eb05efbe6ba53af43194a149
Grace Shea <grace.m.shea@gmail.com>
2019-03-19 15:13:29 -0700

    initial menu drafted for slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -7,6 +7,25 @@ class Slack
 
   def main
     puts "Welcome to the Ada Slack CLI!"
+    
+    puts "Slack CLI Menu\n"
+    puts "1. List Users\n"
+    puts "2. List Channels\n"
+    puts "3. Quit\n"
+
+    loop do
+      "Please enter your selection from the above options."
+      input = gets.chomp.to_s
+      case
+      when input == "1"
+        puts users.list
+      when input == "2"
+        puts channels.list
+      when input == "3"
+        exit
+      when input =~ /[[:alpha:]]/
+        puts "\nYour request was invalid."
+      end
 
     def select_channel
     end
