fe50b30d254e6b78deae41906478887852e038a3
Grace Shea <grace.m.shea@gmail.com>
2019-03-20 15:45:06 -0700

    resolved HTTPS error

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,23 +1,44 @@
-require "httparty"
 require "dotenv"
+require "httparty"
 Dotenv.load
 
 class Recipient
-  BASE_URL = "https://slack.com/api/chat.postMessage"
-  TOKEN = ENV["SLACK_TOKEN"]
+  class SlackApiError < StandardError; end
 
-  def initialize
+  attr_reader :slack_id, :name
+
+  def initialize(slack_id, name)
     @slack_id = slack_id
     @name = name
   end
 
-  # def send_message(message)
-  # end
+  CHANNEL_URL = "https://slack.com/api/channels.list"
+  USER_URL = "https://slack.com/api/users.list"
 
-  # def self.get
-  # end
+  def self.get(type)
+    params = {
+      "token" => ENV["SLACK_TOKEN"],
+    }
+    if type == "user"
+      url = USER_URL
+    elsif type == "channel"
+      url = CHANNEL_URL
+    end
 
-  # def details - request details "package" from user or channel
-  # def list - request list "package" from user or channel
+    response = HTTParty.get(url, query: params)
+  end
 
+  def send_message(message)
+    response = HTTParty.post(url, message?)
+  end
+
+  private
+
+  def details
+    raise NotImplementedError
+  end
+
+  def self.list
+    raise NotImplementedError
+  end
 end
