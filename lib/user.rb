3ed78f2b02a6f050524435b589f1cc745f3980cc
Hana <hanalways@gmail.com>
2019-03-20 14:42:15 -0700

    namespace with a SlackCli module

--- lib/user.rb
+++ lib/user.rb
@@ -2,48 +2,50 @@ require 'pry'
 
 require_relative 'recipient'
 
-class User < Recipient 
-  LIST_URL = "https://slack.com/api/users.list"
-  TOKEN = ENV["SLACK_TOKEN"]
+module SlackCli
+  class User < Recipient 
+    LIST_URL = "https://slack.com/api/users.list"
+    TOKEN = ENV["SLACK_TOKEN"]
 
-  attr_reader :real_name, :status_text, :status_emoji, :list_users, :details
+    attr_reader :real_name, :status_text, :status_emoji, :list_users, :details
 
-  def initialize(name, slack_id, real_name, status_text, status_emoji)
-    @name = name
-    @slack_id = slack_id
-    @real_name = real_name
-    @status_text = status_text 
-    @status_emoji = status_emoji
-  end
-
-  def self.list
-    response = User.get
-
-    users = response["members"].map do |user|
-      name = user["name"]
-      slack_id = user["id"]
-      real_name = user["profile"]["real_name"]
-      status_text = user["profile"]["status_text"]
-      status_emoji = user["profile"]["status_emoji"]
-      self.new(name, slack_id, real_name, status_text, status_emoji)
+    def initialize(name, slack_id, real_name, status_text, status_emoji)
+      @name = name
+      @slack_id = slack_id
+      @real_name = real_name
+      @status_text = status_text 
+      @status_emoji = status_emoji
     end
 
-    return users
-  end
+    def self.list
+      response = SlackCli::User.get
 
-  def self.list_users
-    user_list = list.map do |user|
-      user.name
+      users = response["members"].map do |user|
+        name = user["name"]
+        slack_id = user["id"]
+        real_name = user["profile"]["real_name"]
+        status_text = user["profile"]["status_text"]
+        status_emoji = user["profile"]["status_emoji"]
+        self.new(name, slack_id, real_name, status_text, status_emoji)
+      end
+
+      return users
     end
-    return user_list
-  end
 
-  def self.details
-    user_details = "Username: #{name}
-       Slack ID: #{slack_id}
-       Real Name: #{real_name}
-       Status Text: #{status_text}
-       Status Emoji: #{status_emoji}"
-    return user_details
+    def self.list_users
+      user_list = list.map do |user|
+        user.name
+      end
+      return user_list
+    end
+
+    def self.details
+      user_details = "Username: #{name}
+        Slack ID: #{slack_id}
+        Real Name: #{real_name}
+        Status Text: #{status_text}
+        Status Emoji: #{status_emoji}"
+      return user_details
+    end
   end
 end
--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -1,3 +1,5 @@
+require_relative "test_helper.rb"
+
 describe "User child class" do
   describe "initialize" do
   end
@@ -5,18 +7,18 @@ describe "User child class" do
   describe "user self.get method" do 
     it "successfully returns an HTTParty response object" do
       VCR.use_cassette("user_response") do 
-        expect(User.get["ok"]).must_equal true
+        expect(SlackCli::User.get["ok"]).must_equal true
       end
     end
   end
   describe "user self.list method" do 
     it "creates a list of all user objects" do
       VCR.use_cassette("user_response") do 
-        user_list = User.list
+        user_list = SlackCli::User.list
 
         expect(user_list).must_be_kind_of Array
-        expect(user_list.first).must_be_instance_of User 
-        expect(user_list.last).must_be_instance_of User
+        expect(user_list.first).must_be_instance_of SlackCli::User 
+        expect(user_list.last).must_be_instance_of SlackCli::User
       end
     end
 
@@ -27,17 +29,9 @@ describe "User child class" do
   describe "user self.list_users method" do 
     it "returns all usernames in workspace in array" do
       VCR.use_cassette("user_response") do 
-        expect(User.list_users).must_be_kind_of Array 
-        expect(User.list_users.length).must_equal 3
+        expect(SlackCli::User.list_users).must_be_kind_of Array 
+        expect(SlackCli::User.list_users.length).must_equal 3
       end
     end
   end
-
-  # describe "user self.details method" do
-  #   it "returns a string" do
-  #     VCR.use_cassette("user_response") do
-  #       expect(User.details).must_be_kind_of String  
-  #     end
-  #   end
-  # end
 end
