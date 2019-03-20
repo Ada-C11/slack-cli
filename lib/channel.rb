3e47d87147f646c9ea538e84ebb0f3d58527d3f1
amyesh <amyesh08@gmail.com>
2019-03-20 14:50:58 -0700

    refactoring list methods

--- lib/channel.rb
+++ lib/channel.rb
@@ -14,18 +14,30 @@ class Channel
     @channel_names = []
   end
 
-  def self.list
+  def self.get
     query = {
       token: TOKEN,
     }
     channel_info = HTTParty.get(BASE_URL, query: query)
     if channel_info["ok"] == false
       raise ArgumentError, "The error code is #{channel_info.code} and the reason is: #{channel_info.message}"
-    else
-      channel_list = channel_info["channels"]
-      @channel_names = channel_list.map do |channel|
-        channel["name"]
-      end
+    end
+    return channel_info
+  end
+
+  def self.list
+    # query = {
+    #   token: TOKEN,
+    # }
+    # channel_info = HTTParty.get(BASE_URL, query: query)
+    # if channel_info["ok"] == false
+    #   raise ArgumentError, "The error code is #{channel_info.code} and the reason is: #{channel_info.message}"
+    # else
+    channel_info = Channel.get
+    channel_list = channel_info["channels"]
+    @channel_names = channel_list.map do |channel|
+      channel["name"]
+      # end
       return @channel_names
     end
   end
