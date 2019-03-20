2695c6fc8652c170dd7dfa1bb930f59660ac70be
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-20 14:16:46 -0700

    Wrote tests for Channel#self.list

--- specs/channel_spec.rb
+++ specs/channel_spec.rb
@@ -11,6 +11,16 @@ describe "Channel" do
   end
 
   describe "self.list" do
-    
+    it "can find the list of channels" do
+        VCR.use_cassette("channel_list") do
+          response = SlackAPI::Channel.list
+          expect(response).must_be_kind_of Array
+          expect(response[0]).must_be_kind_of SlackAPI::Channel
+          expect(response[0].name).must_equal "everyone"
+          expect(response[0].slack_id).must_equal "CH2SBU69Y"
+          expect(response[0].topic).must_equal "Company-wide announcements and work-based matters"
+          expect(response[0].member_count).must_equal 2
+         end
+      end
   end
 end
