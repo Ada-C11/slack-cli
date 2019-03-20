01cb46193fcff2c90b5d6ecc7a75d3a496c182d1
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-19 20:38:46 -0700

    added testing for User.list and made it pass

--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -22,15 +22,29 @@ describe "User Class" do
         }.must_raise Slack::Recipient::ResponseError
       end
     end
+  end
 
-    # describe "self.list" do
-    #   it "returns an array" do
-    #     VCR.use_cassette("user_information_find") do
+  describe "self.list" do
+    it "returns an array that contains instances of users" do
+      VCR.use_cassette("user_information_find") do
+        users = Slack::User.list
 
-    #       Slack::User.list("https://slack.com/api/users.list", {token: ENV["KEY"]})
-    #     end
-    #   end
+        expect(users).must_be_kind_of Array
+        (0..users.length - 1).each do |i|
+          expect(users[i]).must_be_kind_of Slack::User
+        end
+      end
+    end
 
-    # end
+    it "loads all user information correctly" do
+      VCR.use_cassette("user_information_find") do
+        users = Slack::User.list
+        expect(users[1].name).must_equal "kimberly.fasbender"
+        expect(users[1].real_name).must_equal "Kim Fasbender"
+        expect(users[1].slack_id).must_equal "UH408BVL7"
+        expect(users.length).must_equal 3
+      end
+    end
+    # # end end
   end
 end
