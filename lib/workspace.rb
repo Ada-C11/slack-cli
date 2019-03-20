8e9df3434c510851dfda373da5a372f58a4a434d
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-20 14:51:30 -0700

    Added menu and quit to workspace.rb

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -9,9 +9,39 @@ def main
   puts "Welcome to the Ada Slack CLI!"
 
   # TODO project
-  puts "This workspace has #{Slack::Channel.list_channels.length} channels."
-  puts "This workspace has #{Slack::User.list_users.length} users."
 
+  puts "\t\t\This workspace has #{Slack::Channel.list_channels.length} channels."
+  puts "\t\t\This workspace has #{Slack::User.list_users.length} users."
+
+  loop do
+    puts "\n\t\t\.:Menu:.
+   |1|::List Channels
+   |2|::Channel Details
+   |3|::List Users
+   |4|::User Details
+   |5|::Select Channel
+   |6|::Select User
+   |7|::Quit"
+
+    answer = gets.chomp.to_i
+
+    # List Stuff
+    case answer
+
+    when 1
+      ap Slack::Channel.list_channels
+    when 2
+    when 3
+      ap Slack::User.list_users
+    when 4
+    when 5
+    when 6
+    when 7
+      break
+    else
+      puts "Please select from Menu Items."
+    end
+  end
   puts "Thank you for using the Ada Slack CLI"
 end
 
