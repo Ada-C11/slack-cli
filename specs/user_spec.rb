deb6d793c9ed1c4e8a73d87ba59db6d5c4ae2faf
Elise Pham <elise.pham88@gmail.com>
2019-03-20 13:52:19 -0700

    added test for details in user class

--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -5,9 +5,24 @@ describe "User class" do
     Slack::User.new(id: "123", name: "sam", real_name: "sam bones")
   }
   describe "User#initialize" do
-    it "Check type is User and type Recipient" do
+    it "Checks type is User and type Recipient" do
       expect(user).must_be_instance_of Slack::User
       expect(user).must_be_kind_of Slack::Recipient
     end
+
+    it "Checks instance variable name, real_name and id: respond & equal to" do
+      expect(user.name).must_equal "sam"
+      expect(user.real_name).must_equal "sam bones"
+      expect(user.id).must_equal "123"
+    end
   end
+
+  # Check details method return String type
+
+  describe "User#details" do
+    it "returns a String" do
+      expect(user.details).must_be_instance_of String
+    end
+  end
+
 end
