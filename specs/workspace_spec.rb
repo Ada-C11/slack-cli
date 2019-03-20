a36f2b733cdf7c77b9f2db0b8bd11733cb969ebb
Erica Norris <norrise120@gmail.com>
2019-03-20 14:21:34 -0700

    Added tests for select_channel and updated tests for select_user

--- specs/workspace_spec.rb
+++ specs/workspace_spec.rb
@@ -22,25 +22,76 @@ describe "SlackCLI::Workspace" do
   end
 
   describe "select_user" do
-    before do 
-      @username = @workspace.users.first.name
-    end
-    it "should return an instance of User, given valid input" do
-      
-      expect(@workspace.select_user(@username)).must_be_kind_of SlackCLI::User
+    it "should return an instance of User, given valid name" do
+      username = @workspace.users.first.name
+      expect(@workspace.select_user(username)).must_be_kind_of SlackCLI::User
       expect(@workspace.selected).must_be_kind_of SlackCLI::User
       expect(@workspace.selected).must_equal @workspace.users.first
     end
-    # add after select_channel is implemented
-    # it "should update selected when a new user is selected" do
-    #   @workspace.select_channel(channel)
-    #   username2 = @workspace.users[1].name
-    #   expect(@workspace.selected).must_equal @workspace.users[1]
-    # end
+
+    it "should return an instance of User, given valid slack id" do
+      slack_id = @workspace.users.first.slack_id
+      expect(@workspace.select_user(slack_id)).must_be_kind_of SlackCLI::User
+      expect(@workspace.selected).must_be_kind_of SlackCLI::User
+      expect(@workspace.selected).must_equal @workspace.users.first
+    end
+
+    it "should update selected when a new user is selected" do
+      slack_id1 = @workspace.users.first.slack_id
+      slack_id2 = @workspace.users.last.slack_id
+      @workspace.select_user(slack_id1)
+      expect(@workspace.selected).must_equal @workspace.users.first
+      @workspace.select_user(slack_id2)
+      if slack_id1 != slack_id2
+        expect(@workspace.selected).must_equal @workspace.users.last
+      end
+    end
 
     it "should return nil, given invalid user" do
       bad_username = ""
       @workspace.select_user(bad_username)
+      expect(@workspace.selected).must_be_nil
+    end
+  end
+
+  describe "select_channel" do
+    it "should return an instance of Channel, given valid name" do
+      name = @workspace.channels.first.name
+      expect(@workspace.select_channel(name)).must_be_kind_of SlackCLI::Channel
+      expect(@workspace.selected).must_be_kind_of SlackCLI::Channel
+      expect(@workspace.selected).must_equal @workspace.channels.first
+    end
+
+    it "should return an instance of Channel, given valid slack id" do
+      slack_id = @workspace.channels.first.slack_id
+      expect(@workspace.select_channel(slack_id)).must_be_kind_of SlackCLI::Channel
+      expect(@workspace.selected).must_be_kind_of SlackCLI::Channel
+      expect(@workspace.selected).must_equal @workspace.channels.first
+    end
+
+    it "should update selected when a new channel is selected" do
+      slack_id1 = @workspace.channels.first.slack_id
+      slack_id2 = @workspace.channels.last.slack_id
+      @workspace.select_channel(slack_id1)
+      expect(@workspace.selected).must_equal @workspace.channels.first
+      @workspace.select_channel(slack_id2)
+      if slack_id1 != slack_id2
+        expect(@workspace.selected).must_equal @workspace.channel.last
+      end
+    end
+
+    it "should update selected from user to channel when new channel is selected" do
+      user_slack_id = @workspace.users.first.slack_id
+      channel_slack_id = @workspace.channels.first.slack_id
+      @workspace.select_user(user_slack_id)
+      expect(@workspace.selected).must_equal @workspace.users.first
+      @workspace.select_channel(channel_slack_id)
+      expect(@workspace.selected).must_equal @workspace.channel.first
+    end
+
+    it "should return nil, given invalid channel" do
+      bad_slack_id = ""
+      @workspace.select_channel(bad_slack_id)
       expect(@workspace.selected).must_be_nil
     end
   end
