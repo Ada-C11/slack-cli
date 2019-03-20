9bcd182817fa83cb5fb0062f97e3c9daa1d8bf94
Cloudy Lopez <cloudylopez@gmail.com>
2019-03-20 15:32:08 -0700

    completed wave one implemented loop and all wave one requirement have been tested and run properly

--- lib/slack.rb
+++ lib/slack.rb
@@ -11,22 +11,23 @@ def main
   channel = Slack::Channel
   
   puts "Welcome to the Ada Slack CLI!"
-  puts "#{users.list.length} users and #{"PUT IN CHANNELS"} channels were uploaded.\n\n"
+  selection = 1
+  while selection != 3
+    puts "#{users.list.length} users and #{channel.list.length} channels were uploaded.\n\n"
 
   puts "Please make a selection:\n1. list users\n2. list channels\n3. quit\n\n"
   selection = gets.chomp.to_i
 
-  # tp Author.limit(3), "name", "books.title", "books.photos.caption"
   if selection == 1
     # users.list.each do |user| user.real_name
     tp users.list, "slack_id", "name", "real_name"
   elsif selection == 2
     tp channel.list, "slack_id", "name", "topic", "member_count"
   elsif selection == 3
+    puts "Thank you for using the Ada Slack CLI"
     exit
   end # end
-
-  puts "Thank you for using the Ada Slack CLI"
+  end
 end
 
 # end
