112cd39bba6d3b7fc75c4aef21c4dc08659bd55e
Kelly Downes <kedownes@gmail.com>
2019-03-20 12:31:46 -0700

    Add functionality to list users and channels

--- lib/slack.rb
+++ lib/slack.rb
@@ -9,22 +9,33 @@ def main
   puts "There are #{workspace.users.count} users in this workspace"
 
   puts "You can: List Users, List Channels, or Quit."
-  puts "Please enter what option you would like to take.  You can exit by entering 'quit'."
+  puts "Please enter what option you would like to take.  You can quit by entering 'quit'."
   response = gets.chomp.downcase
   while response != "quit"
     if response == "list users"
-      puts workspace.users
+      list_users(workspace)
     elsif response == "list channels"
-      planet_choice = solar_system.find_planet_by_name(gets.chomp)
-      puts workspace.channels
+      list_channels(workspace)
     else
       puts "Please make sure to select from the choices above. You can quit be entering 'quit'."
     end
-    puts "\nPlease enter what you would like to do: \nList Users \nList Channels"
+    puts "\nPlease enter what you would like to do: \nList Users \nList Channels \nQuit"
     response = gets.chomp.downcase
   end
 
   puts "Thank you for using the Ada Slack CLI"
+end
+
+def list_users(workspace)
+  workspace.users.each do |user|
+    puts "Slack ID: #{user.slack_id}, username: #{user.name}, real name: #{user.real_name}"
+  end
+end
+
+def list_channels(workspace)
+  workspace.channels.each do |channel|
+    puts "Slack ID: #{channel.slack_id}, name: #{channel.name}, topic: #{channel.topic}, member count: #{channel.member_count}"
+  end
 end
 
 main if __FILE__ == $PROGRAM_NAME
