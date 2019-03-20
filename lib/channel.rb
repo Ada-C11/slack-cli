6d4c6adc60f0fdafdda0e67d879db6fe2227d216
Shirley Chu <shirley.cchu@gmail.com>
2019-03-20 14:44:50 -0700

    Wrote Channel.details and subsequent tests

--- lib/channel.rb
+++ lib/channel.rb
@@ -13,7 +13,7 @@ module Slack
 
     attr_reader :slack_id, :name, :topic, :member_count
 
-    def initialize(slack_id, name, topic, member_count)
+    def initialize(slack_id:, name:, topic:, member_count:)
       @slack_id = slack_id
       @name = name
       @topic = topic
@@ -22,15 +22,24 @@ module Slack
 
     def self.list
       response = self.get(CHANNEL_URL, query: @query)
-      #   binding.pry
+      # binding.pry
       channels = response["channels"].map do |channel|
         id = channel["id"]
         name = channel["name"]
         topic = channel["topic"]["value"]
         member_count = channel["num_members"]
-        self.new(id, name, topic, member_count)
+        self.new(slack_id: id, name: name, topic: topic, member_count: member_count)
       end
       return channels
+    end
+
+    def details
+      return {
+               slack_id: slack_id,
+               name: name,
+               topic: topic,
+               member_count: member_count,
+             }
     end
   end
 end
--- specs/channel_spec.rb
+++ specs/channel_spec.rb
@@ -3,25 +3,56 @@ require_relative "test_helper"
 describe "Channel" do
   describe "self.list" do
     before do
-      VCR.use_cassette("make channels") do
+      VCR.use_cassette("make channels list") do
         query_params = {
           token: KEY,
         }
         @channels = Slack::Channel.list
+        @channel = @channels.first
       end
     end
 
     it "will return an array of Channels" do
       expect(@channels).must_be_kind_of Array
-      expect(@channels[0]).must_be_kind_of Slack::Channel
+      expect(@channel).must_be_kind_of Slack::Channel
     end
 
     it "will assign the correct attributes to each Channel" do
-      channel = @channels[0]
-      expect(channel.slack_id).must_equal "CH2V3FHEY"
-      expect(channel.name).must_equal "general"
-      expect(channel.topic).must_equal "Company-wide announcements and work-based matters"
-      expect(channel.member_count).must_equal 2
+      expect(@channel.slack_id).must_equal "CH2V3FHEY"
+      expect(@channel.name).must_equal "general"
+      expect(@channel.topic).must_equal "Company-wide announcements and work-based matters"
+      expect(@channel.member_count).must_equal 2
+    end
+
+    it "has the correct data tyes for each attribute" do
+      expect(@channel.slack_id).must_be_kind_of String
+      expect(@channel.name).must_be_kind_of String
+      expect(@channel.topic).must_be_kind_of String
+      expect(@channel.member_count).must_be_kind_of Integer
+    end
+  end
+
+  describe "details" do
+    before do
+      VCR.use_cassette("show channel details") do
+        query_params = {
+          token: KEY,
+        }
+        @channels = Slack::Channel.list
+        @channel = @channels.first
+      end
+    end
+
+    it "returns the correct data" do
+      details = @channel.details
+      expected_details = {
+        slack_id: @channel.slack_id,
+        name: @channel.name,
+        topic: @channel.topic,
+        member_count: @channel.member_count,
+      }
+
+      expect(details).must_equal expected_details
     end
   end
 end
