1a287b7cf8fe59b27bd8b1cc71bf8f00d13d7d35
Erica Norris <norrise120@gmail.com>
2019-03-20 12:06:44 -0700

    Updated Slack.rb to ask user for input and list possible options so far. Need to update output for the options

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,29 @@
 #!/usr/bin/env ruby
 require_relative "workspace.rb"
+
 def main
   puts "Welcome to the Ada Slack CLI!"
 
   workspace = SlackCLI::Workspace.new
   puts "There are #{workspace.channels.count} channels in this workspace"
   puts "There are #{workspace.users.count} users in this workspace"
+
+  puts "You can: List Users, List Channels, or Quit."
+  puts "Please enter what option you would like to take.  You can exit by entering 'quit'."
+  response = gets.chomp.downcase
+  while response != "quit"
+    if response == "list users"
+      puts workspace.users
+    elsif response == "list channels"
+      planet_choice = solar_system.find_planet_by_name(gets.chomp)
+      puts workspace.channels
+    else
+      puts "Please make sure to select from the choices above. You can quit be entering 'quit'."
+    end
+    puts "\nPlease enter what you would like to do: \nList Users \nList Channels"
+    response = gets.chomp.downcase
+  end
+
   puts "Thank you for using the Ada Slack CLI"
 end
 
