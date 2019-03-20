9258eb5aa74a265ceef298a569fc87fa4b81a119
Bitaman <b.amani.s@gmail.com>
2019-03-20 14:00:52 -0700

    merging

--- lib/user.rb
+++ lib/user.rb
@@ -1,23 +1,15 @@
 
 require "httparty"
-<<<<<<< HEAD
 require "pry"
 require_relative "recipient"
 require "dotenv"
 Dotenv.load
-=======
-require_relative "recipient"
->>>>>>> refs/remotes/origin/master
 
 class User < Recipient
   BASE_URL = "https://slack.com/api/users.list"
   TOKEN = ENV["SLACK_TOKEN"]
 
-<<<<<<< HEAD
   def initialize
-=======
-  def initialize(slack_id, name)
->>>>>>> refs/remotes/origin/master
     super
     @real_name = real_name
     @status_text = status_text
@@ -34,12 +26,12 @@ class User < Recipient
 
     unless response.code == 200
       raise SearchError, "Cannot find #{search_term}"
-     end
+    end
     # response.each do |response|
     #   puts "id: #{response.first["members"][0]}"
     # end
     return response
-   end
+  end
 end
 
 #puts User.list
