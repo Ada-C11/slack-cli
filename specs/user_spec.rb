ecada61a721419fb3676bde42a1df120c7f9a63e
qqdipps <sdipp007@gmail.com>
2019-03-20 13:41:27 -0700

    added tests for initialize User class.

---
+++ specs/user_spec.rb
@@ -0,0 +1,13 @@
+require_relative "test_helper"
+
+describe "User class" do
+  let(:user) {
+    Slack::User.new(id: "123", name: "sam", real_name: "sam bones")
+  }
+  describe "User#initialize" do
+    it "Check type is User and type Recipient" do
+      expect(user).must_be_instance_of Slack::User
+      expect(user).must_be_kind_of Slack::Recipient
+    end
+  end
+end
