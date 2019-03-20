b8d80513cd1953447b2bf82a8decca05185e6065
Grace Shea <grace.m.shea@gmail.com>
2019-03-20 13:58:39 -0700

    channel list functioning

--- lib/channel.rb
+++ lib/channel.rb
@@ -3,17 +3,10 @@ require_relative "recipient"
 require "dotenv"
 Dotenv.load
 
-# class Channel < Recipient
-#   BASE_URL = "https://slack.com/api/channels.list"
-#   TOKEN = ENV["SLACK_TOKEN"]
+class Channel < Recipient
+  BASE_URL = "https://slack.com/api/channels.list"
+  TOKEN = ENV["SLACK_TOKEN"]
 
-<<<<<<< HEAD
-#   def initialize(slack_id, name, topic, member_count)
-#     super
-#     @topic = topic
-#     @member_count = member_count
-#   end
-=======
   class SlackAPIError < StandardError; end
 
   def initialize
@@ -21,16 +14,10 @@ Dotenv.load
     @topic = topic
     @member_count = member_count
   end
->>>>>>> refs/remotes/origin/master
 
-#   def details
-#   end
+  #   def details
+  #   end
 
-<<<<<<< HEAD
-#   def self.list
-#   end
-# end
-=======
   def self.list
     query_params = {
       token: TOKEN,
@@ -52,4 +39,3 @@ end
 puts Channel.list
 
 # , "API call failed with code #{response.code} and reason '#{response["reason"]}"
->>>>>>> refs/remotes/origin/master
--- lib/slack.rb
+++ lib/slack.rb
@@ -5,10 +5,9 @@ require "httparty"
 #   def initialize
 #   end
 
-<<<<<<< HEAD
 #   def main
 #     puts "Welcome to the Ada Slack CLI!"
-    
+
 #     puts "Slack CLI Menu\n"
 #     puts "1. List Users\n"
 #     puts "2. List Channels\n"
@@ -45,45 +44,3 @@ require "httparty"
 #   end
 
 #   main if __FILE__ == $PROGRAM_NAME
-=======
-  def main
-    puts "Welcome to the Ada Slack CLI!"
-
-    # puts "Slack CLI Menu\n"
-    # puts "1. List Users\n"
-    # puts "2. List Channels\n"
-    # puts "3. Quit\n"
-
-    # loop do
-    #   "Please enter your selection from the above options."
-    #   input = gets.chomp.to_s
-    #   case
-    #   when input == "1"
-    #     puts users.list
-    #   when input == "2"
-    #     puts channels.list
-    #   when input == "3"
-    #     exit
-    #   when input =~ /[[:alpha:]]/
-    #     puts "\nYour request was invalid."
-    #   end
-    # end
-
-    def select_channel
-    end
-
-    def select_user
-    end
-
-    def show_details
-    end
-
-    def send_message
-    end
-
-    puts "Thank you for using the Ada Slack CLI"
-  end
-
-  main if __FILE__ == $PROGRAM_NAME
-end
->>>>>>> refs/remotes/origin/master
