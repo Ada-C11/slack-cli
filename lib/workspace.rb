b5478f3cc90e4c2d897aca56ba1a6ec27b3713d2
Erica Norris <norrise120@gmail.com>
2019-03-20 14:04:57 -0700

    Created select_user and find_user for workspace class, passed tests

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -10,5 +10,20 @@ module SlackCLI
       @channels = SlackCLI::Channel.list
       @selected = nil
     end
+
+    def select_user(user_info)
+      @selected = find_user(user_info)
+    end
+
+    private
+
+    def find_user(user_info)
+      @users.each do |user|
+        if user_info == user.slack_id || user_info == user.name
+          return user
+        end
+      end
+      return nil
+    end
   end
 end
