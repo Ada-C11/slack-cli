ea4fe70464dadb135c76859b4231ecb7a1d8647e
Elise Pham <elise.pham88@gmail.com>
2019-03-20 14:14:26 -0700

    added test for Channel#details

--- specs/channel_spec.rb
+++ specs/channel_spec.rb
@@ -1,10 +1,11 @@
 require_relative "test_helper.rb"
 
 describe "Channel class" do
+  let(:channel) {
+    Slack::Channel.new(id: "CBD", name: "general", topic: "stuff", member_count: "4")
+  }
+  
   describe "Channel#initialize" do
-    let(:channel) {
-      Slack::Channel.new(id: "CBD", name: "general", topic: "stuff", member_count: "4")
-    }
     it "returns an instance of Channel and a kind of Recipient" do
       expect(channel).must_be_instance_of Slack::Channel
       expect(channel).must_be_kind_of Slack::Recipient
@@ -20,6 +21,12 @@ describe "Channel class" do
       expect(channel.id).must_equal "CBD"
       expect(channel.topic).must_equal "stuff"
       expect(channel.member_count).must_equal "4"
+    end
+  end
+
+  describe "Channel#details" do
+    it "returns a String" do
+      expect(channel.details).must_be_instance_of String
     end
   end
 end
