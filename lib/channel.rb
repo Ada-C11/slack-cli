088b575eedf27d650b68f25ef882b04fa44b2d27
qqdipps <sdipp007@gmail.com>
2019-03-20 14:15:15 -0700

    added details method signature to channels

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,11 +1,15 @@
 module Slack
   class Channel < Recipient
     attr_reader :member_count, :topic
-    
+
     def initialize(id:, name:, member_count:, topic:)
       super(id: id, name: name)
       @member_count = member_count
       @topic = topic
+    end
+
+    def details
+      return ""
     end
   end
 end
