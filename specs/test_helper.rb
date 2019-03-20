0e2c0fa3801c581c6bd6f68fbb934612b17b2c9d
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-03-20 13:37:09 -0700

    updated test_helper file to include VCR configuration

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -1,15 +1,26 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'dotenv'
+Dotenv.load
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require 'webmock/minitest'
 require 'vcr'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "specs/cassettes"
+  config.cassette_library_dir = 'specs/cassettes'
   config.hook_into :webmock
+  config.default_cassette_options = {
+    record: :new_episodes, 
+    match_requests_on: [:method, :uri, :body] 
+  }
+  config.filter_sensitive_data("SLACK_API_TOKEN") do
+    ENV["SLACK_API_TOKEN"]
+  end
 end
