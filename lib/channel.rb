bc9cc58491721fc8e2eb4d54aa51ac6840fbb7a8
Tatiana Quintana <tatsqui@gmail.com>
2019-03-20 14:52:54 -0700

    replaced missing end

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,33 +1,34 @@
 require_relative "recipient"
 
 module SlackCli
-class Channel < SlackCli::Recipient
-  attr_reader :topic, :member_count
-  LIST_URL = "https://slack.com/api/channels.list"
-  MSG_URL = ""
+  class Channel < SlackCli::Recipient
+    attr_reader :topic, :member_count
+    LIST_URL = "https://slack.com/api/channels.list"
+    MSG_URL = ""
 
-  def initialize(slack_id, name, topic, member_count)
-    @slack_id = slack_id
-    @name = name
-    @topic = topic
-    @member_count = member_count
-  end
-
-  def self.list
-    response = SlackCli::Channel.get
-
-    channels = response["channels"].map do |channel|
-      slack_id = channel["id"]
-      name = channel["name"]
-      topic = channel["topic"]["value"]
-      member_count = channel["num_members"]
-      self.new(slack_id, name, topic, member_count)
+    def initialize(slack_id, name, topic, member_count)
+      @slack_id = slack_id
+      @name = name
+      @topic = topic
+      @member_count = member_count
     end
-    return channels
-  end
 
-  def details
-    return "Channel name: #{name} \nID: #{slack_id} \ntopic: #{topic}, \nMember count:#{member_count}\n"
+    def self.list
+      response = SlackCli::Channel.get
+
+      channels = response["channels"].map do |channel|
+        slack_id = channel["id"]
+        name = channel["name"]
+        topic = channel["topic"]["value"]
+        member_count = channel["num_members"]
+        self.new(slack_id, name, topic, member_count)
+      end
+      return channels
+    end
+
+    def details
+      return "Channel name: #{name} \nID: #{slack_id} \ntopic: #{topic}, \nMember count:#{member_count}\n"
+    end
   end
 end
 
