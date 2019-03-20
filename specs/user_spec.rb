58b7b462c383a96ed119c7d6d0c68d5f366045b7
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-20 15:25:18 -0700

    added tests to check count of users returned

--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -11,28 +11,51 @@ describe "user class" do
   end
 
   describe "can connect to API" do
+    before do
+      VCR.use_cassette("connect to endpoints users_list") do
+        endpoint = "users.list"
+        @response = User.get(endpoint)
+      end
+    end
     it "gives a list of three names" do
       VCR.use_cassette("find members") do
-        endpoint = "users.list"
-        response = User.get(endpoint)
-        expect(response).wont_be_nil
-        expect(response["members"].map { |member| member["name"] }).must_equal ["slackbot", "wmcarmelina", "cyndilopez6"]
+        expect(@response).wont_be_nil
+        expect(@response["members"].map { |member| member["name"] }).must_equal ["slackbot", "wmcarmelina", "cyndilopez6"]
       end
     end
     it "correctly finds the status of a member" do
       VCR.use_cassette("user status") do
-        endpoint = "users.list"
-        response = User.get(endpoint)
-        expect(response["members"][0]["profile"]["status_text"].length).must_equal 0
-        expect(response["members"].select { |member| member["real_name"] == "Maria Wissler" }[0]["profile"]["status_text"]).must_equal "Working remotely from Kauai"
+        expect(@response["members"][0]["profile"]["status_text"].length).must_equal 0
+        # expect(@response["members"].select { |member| member["real_name"] == "Maria Wissler" }[0]["profile"]["status_text"]).must_equal "Working remotely from Kauai"
       end
     end
-    describe "create list of users" do
-      it "returns an array with instance of user" do
-        VCR.use_cassette("user status") do
-          user_list = User.list
-          expect(user_list).must_be_kind_of Array
-        end
+  end
+  describe "create list of users" do
+    before do
+      VCR.use_cassette("list_users") do
+        @user_list = User.list
+      end
+    end
+    it "returns an array of users" do
+      VCR.use_cassette("list_users") do
+        expect(@user_list).must_be_kind_of Array
+      end
+    end
+    it "contains instances of user within the array" do
+      VCR.use_cassette("instance of User within array") do
+        expect(@user_list[0]).must_be_kind_of User
+      end
+    end
+    # can this be used for other channel?
+    it "returns a list with length 3" do
+      VCR.use_cassette("length user list") do
+        expect(@user_list.length).must_equal 3
+      end
+    end
+
+    it "returns name of first user correctly" do
+      VCR.use_cassette("length user list") do
+        expect(@user_list.first.real_name).must_equal "Slackbot"
       end
     end
   end
