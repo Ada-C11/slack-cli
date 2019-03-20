dd88f725c0e12c48d6e5b6aa95a68949f997860a
qqdipps <sdipp007@gmail.com>
2019-03-20 14:10:06 -0700

    added tests for instance vars

--- specs/channel_spec.rb
+++ specs/channel_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'test_helper.rb'
+require_relative "test_helper.rb"
 
 describe "Channel class" do
   describe "Channel#initialize" do
@@ -8,6 +8,18 @@ describe "Channel class" do
     it "returns an instance of Channel and a kind of Recipient" do
       expect(channel).must_be_instance_of Slack::Channel
       expect(channel).must_be_kind_of Slack::Recipient
+    end
+
+    it "Checks instance variable name, id, topic, member_count: respond & equal to" do
+      expect(channel).must_respond_to :name
+      expect(channel).must_respond_to :id
+      expect(channel).must_respond_to :topic
+      expect(channel).must_respond_to :member_count
+
+      expect(channel.name).must_equal "general"
+      expect(channel.id).must_equal "CBD"
+      expect(channel.topic).must_equal "stuff"
+      expect(channel.member_count).must_equal "4"
     end
   end
 end
