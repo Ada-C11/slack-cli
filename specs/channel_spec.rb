d157d554b0e94e965691f5bf26783769cc244c21
Ngoc Le <ngocle@outlook.com>
2019-03-20 09:25:50 -0700

    added test to channel_spec

--- specs/channel_spec.rb
+++ specs/channel_spec.rb
@@ -1,28 +1,12 @@
 require_relative "test_helper"
-<<<<<<< HEAD
-=======
 
 describe "Channel class" do
   describe "Channel#list" do
-    it "finds channels" do
-      VCR.use_cassette("Channel") do
-        location = "Seattle"
-        response = get_location(location)
-
-        expect(response["Seattle"]).wont_be_nil
-        expect(response["Seattle"][:lon]).must_equal "-122.3300624"
-        expect(response["Seattle"][:lat]).must_equal "47.6038321"
-      end
-    end
-
     it "will raise an exception if the search fails" do
       VCR.use_cassette("Channel") do
-        location = ""
-        expect {
-          response = get_location(location)
-        }.must_raise SearchError
+        response = Channel.list
+        expect { response }.must_include "general"
       end
     end
   end
 end
->>>>>>> fb61b11586278c3c4324ec484a7ccbb159320108
