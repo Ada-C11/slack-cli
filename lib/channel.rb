f24482957162bd7f4623283c733561be052f7e11
Jansen <v.jansen.martin@gmail.com>
2019-03-20 14:40:45 -0700

    Revise self.list method, add tests

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,5 +1,5 @@
 require_relative "recipient"
-require 'pry'
+require "pry"
 require "dotenv"
 require "httparty"
 
@@ -7,13 +7,14 @@ Dotenv.load
 
 module Slack
   class Channel < Recipient
+    attr_reader :topic, :member_count
+
     def initialize(topic:, member_count:)
       @topic = topic
       @member_count = member_count
     end
 
     CHANNEL_URL = "https://slack.com/api/channels.list"
-
 
     def details
 
@@ -23,17 +24,19 @@ module Slack
 
     def self.list
       query_params = { token: ENV["SLACK_API_TOKEN"] }
-    
+
       response = Slack::Channel.get(CHANNEL_URL, query_params)
-      channels = response["channels"]
-      channels.each do |channel|
-        puts channel["name"] 
-        puts channel["id"]
-        puts channel["topic"]["value"]
-        puts channel["members"].count
+      channels = response["channels"].map do |channel|
+        name = channel["name"]
+        id = channel["id"]
+        topic = channel["topic"]["value"]
+        member_count = channel["members"].count
+
+        Slack::Channel.new(topic: topic, member_count: member_count)
       end
+
+      return channels
     end
-    puts Slack::Channel.list
   end
 end
 
---
+++ specs/channel_spec.rb
@@ -0,0 +1,21 @@
+require "simplecov"
+SimpleCov.start
+
+require_relative "test_helper"
+require_relative "../lib/channel.rb"
+require_relative "../lib/slack_main.rb"
+
+describe "Channel class" do
+  before do
+    VCR.use_cassette("channel_list") do
+      query_params = { token: ENV["SLACK_API_TOKEN"] }
+      @response = Slack::Channel.list
+    end
+  end
+
+  it "can retrieve a list of channels" do
+    expect(@response).must_be_kind_of Array
+    expect(@response[0]).must_be_kind_of Slack::Channel
+    expect(@response[0].member_count).must_be_kind_of Integer
+  end
+end
