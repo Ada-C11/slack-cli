7e652fb462b3676eadc786bca72b8298f0cde6fd
MyriamWD <myriam.waldend@gmail.com>
2019-03-21 09:18:28 -0700

    has the required relatives for User class to work

--- specs/test_helper.rb
+++ specs/test_helper.rb
@@ -1,6 +1,7 @@
 require "simplecov"
 SimpleCov.start
 
+require "minitest/skip_dsl"
 require "httparty"
 require "minitest"
 require "minitest/autorun"
@@ -14,8 +15,13 @@ Dotenv.load
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "specs/cassettes"
-  config.hook_into :webmock
+  config.cassette_library_dir = "specs/cassettes" # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body], # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our token lying around in a cassette file.
   config.filter_sensitive_data("<SLACK_API_TOKEN>") do
     ENV["SLACK_API_TOKEN"]
   end
@@ -23,5 +29,7 @@ end
 
 require_relative "../lib/recipient.rb"
 require_relative "./recipient_spec.rb"
-# require "users_spec.rb"
+require "./lib/user.rb"
+require "./specs/user_specs.rb"
+
 # require "channels_spec.rb"
