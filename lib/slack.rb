4cabb7c82542d86da7c3d6f0ce20785a012e1573
Cyndi Lopez <cyndilopez6@gmail.com>
2019-03-20 22:02:56 -0700

    pseudocode cli

--- lib/slack.rb
+++ lib/slack.rb
@@ -11,6 +11,12 @@ def main
   puts "What would you like to do? Type 1 to select a user or 2 to quit"
   option = gets.chomp()
   return if option == 2
+
+  #if option = #, select_user
+  #username = gets.chomp
+  #workspace = Workspace.new(selected: username)
+  #if select show_details, workspace.show_details
+
   puts "Thank you for using the Ada Slack CLI"
 end
 
