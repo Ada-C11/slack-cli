803b05153458ca8e3100d257451a9ab33e256cde
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-21 15:25:33 -0700

    Updated driver code for select user & channel

--- lib/slack.rb
+++ lib/slack.rb
@@ -18,30 +18,58 @@ def main
   input = 0
 
   users = SlackAPI::User.load
-  channels SlackAPI::Channel.load
+  channels = SlackAPI::Channel.load
   workspace = SlackAPI::Workspace.new(users: users, channels: channels)
 
-  until input == "quit" do
+  until input == "E" do
     puts "Welcome to the Ada Slack CLI!"
     puts "What would you like to do?"
-    puts "list users"
-    puts "list channels"
-    puts "quit"
-    input = gets.chomp.downcase
-    valid_input = ["list users", "list channels", "select user", "select channel", "quit"]
+    puts "[A] List users"
+    puts "[B] List channels"
+    puts "[C] Select user"
+    puts "[D] Select channel"
+    puts "[E] Quit"
+    input = gets.chomp.upcase
+    valid_input = ["A", "B", "C", "D", "E"]
 
     until valid_input.include?(input)
       puts "Please try again"
-      input = gets.chomp.downcase
+      input = gets.chomp.upcase
     end
 
     case input
-    when "list users"
-      tp SlackAPI::User.list
-    when "list channels"
-      tp SlackAPI::Channel.list
-    else 
-
+      when "A"
+        tp SlackAPI::User.list
+      when "B"
+        tp SlackAPI::Channel.list
+      when "C"
+        puts "Please supply a slack ID or user name (case sensitive!)"  
+        # Resets selected
+        workspace.selected = nil
+        # Needs error checking
+        user_id_or_name = gets.chomp
+        workspace.select_user(id_or_name: user_id_or_name)
+        puts "[A] show details for #{user_id_or_name}"
+        puts "[B] Go back to main menu"
+        # Needs error checking
+        user_details_input = gets.chomp.upcase
+        if user_details_input == "A"
+          puts workspace.show_details
+        # All other input goes back to main menu
+        end
+      when "D"
+        puts "Please supply a slack ID or user name (case sensitive!)"
+        workspace.selected = nil
+        chan_id_or_name = gets.chomp.downcase
+        workspace.select_channel(id_or_name: chan_id_or_name)
+        puts "[A] show details for #{chan_id_or_name}"
+        puts "[B] Go back to main menu"
+        # Needs error checking
+        chan_details_input = gets.chomp.upcase
+        if chan_details_input == "A"
+          puts workspace.show_details
+        # All other input goes back to main menu
+        end
     end
 
     puts "Thank you for using the Ada Slack CLI"
