d44f0b063df5c840b18eeca24b11aba0cc1121c7
Amy Phung <phung.amy85@gmail.com>
2019-03-20 17:34:01 -0700

    added get method to Channel.rb

--- lib/channel.rb
+++ lib/channel.rb
@@ -6,12 +6,26 @@ class Channel < Recipient
   
   def initialize
     # super(slack_id, name)
-    @topic
-    @member_count
+    @topic = topic
+    @member_count = member_count
+    @name = name
   end
   
   def self.get(url, params)
+    response = HTTParty.get(url, query: params)
     
+    if response["ok"] == false
+      raise SlackApiError, "API call failed with reason #{response["error"]}"
+    end
+    
+    channels = response["channels"].map do |channel|
+      name = channel["name"]
+      topic = channel["topic"]
+      member_count = channel["members"].count
+      
+      self.new(name: name, topic: topic, member_count: member_count)
+    end
+    return channels
   end
   
   def self.list
