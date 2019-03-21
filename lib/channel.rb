cce321684416ed8620bdbfa73b3fa358185825b6
Chantal Demissie <chantaldemissie@yahoo.com>
2019-03-21 15:18:16 -0700

    initialized channel

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,12 +1,23 @@
+require 'table_print'
+require "httparty"
+require "dotenv"
+
 class Channel
+  # @topic = topic
+  # @member_count = member_count
 
-@topic = topic
-@member_count = member_count
+  def initialize(channel_map)
+    @id = channel_map['id']
+    @topic = channel_map['topic']['value']
+    @member_count = channel_map['num_members']
+    @name = channel_map['name']
+  end
 
-def details
-  #code here
-end
-
-def self.list
-  #code here
+  # def details
+  #   #code here
+  # end
+  #
+  # def self.list
+  #   #code here
+  # end
 end
