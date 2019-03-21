b866ff124410be42211cfb5d776e2bdf48655fdb
Chantal Demissie <chantaldemissie@yahoo.com>
2019-03-21 15:36:35 -0700

    initialized user

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,17 @@
 class User
-
-  @real_name = real_name
-  #@status_text = status_text
-  #@status_emoji = status_emoji
-
-  def details do
-    #code here
+  def initialize(user_map)
+    @username = user_map['display_name']
+    @real_name = user_map['real_name']
+    @slack_id = user_map['id']
+    # @status_text = user_map['status_text']
+    # @status_emoji = user_map['status_emoji']
   end
 
-  def self.list
-    #code here
-  end 
+  # def details do
+  #   #code here
+  # end
+  #
+  # def self.list
+  #   #code here
+  # end
+end
