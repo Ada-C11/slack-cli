b230f924ed62f7959d9d130a3d79bd36538a7575
Tatiana Quintana <tatsqui@gmail.com>
2019-03-21 13:19:34 -0700

    moved driver code into slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,23 +1,56 @@
-# #!/usr/bin/env ruby
-# require "dotenv"
-# require "httparty"
-# require "pry"
-# require "awesome_print"
-# Dotenv.load
+require_relative 'workspace'
 
-# def main
-#   puts "Welcome to the Ada Slack CLI!"
+#driver code
+def main
+  workspace = Workspace.new
+  puts "TatiHana Slack Channels loaded #{SlackCli::Channel.list.length} channels"
+  puts "TatiHana Users loaded #{SlackCli::User.list.length} users"
 
-#   url = "https://slack.com/api/channels.list"
+  def options
+    puts "Please choose one of the following options:
+    - list users
+    - list channels
+    - select user
+    - select channel
+    - details
+    - send message
+    - quit
+    Enter your choice now:"
+    # selected = false
+  end
 
-#   query_params = { token: ENV["API_KEY"] }
+  options
+  choice = gets.chomp
 
-#   response = HTTParty.get(url, query: query_params)
+  loop do
+    case choice
+    when "list users"
+      puts workspace.list_users
+    when "list channels"
+      puts workspace.list_channels
+    when "select user"
+      puts "what user would you like to select?"
+      selected_user = gets.chomp
+      workspace.select_user(selected_user)
+    when "select channel"
+      puts "What channel would you like to select?"
+      selected_channel = gets.chomp
+      workspace.select_channel(selected_channel)
+    when "details"
+      puts "Details for #{workspace.selected.name}..."
+      workspace.show_details
+    when "send message"
+      workspace.send_messagea
+    when "quit"
+      puts "Thanks for checking out TatiHana! Bye bye..."
+      exit
+    else
+      puts "Invalid option! :("
+    end
 
-#   response["channels"].each do |channel|
-#     puts channel["name"]
-#   end
-#   puts "Thank you for using the Ada Slack CLI"
-# end
+    options
+    choice = gets.chomp
+  end
+end
 
-# main if __FILE__ == $PROGRAM_NAME
+main if __FILE__ == $0
