43b9e789fce17c395556f8529957d946ce3c2bd9
qqdipps <sdipp007@gmail.com>
2019-03-21 14:44:27 -0700

    added test for instance vars.

--- specs/workspace_spec.rb
+++ specs/workspace_spec.rb
@@ -11,5 +11,12 @@ describe "Workspace class" do
     it "returns a workspace object" do
       expect(@workspace).must_be_instance_of Slack::Workspace
     end
+
+    it "responds to instance variable and selected is nil" do
+      expect(@workspace).must_respond_to :channels
+      expect(@workspace).must_respond_to :users
+      expect(@workspace).must_respond_to :selected
+      expect(@workspace.selected).must_be_nil
+    end
   end
 end
