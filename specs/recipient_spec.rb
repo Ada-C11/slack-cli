6b0725b0e60da840a7875ea9d7be4408064973d0
Tatiana Quintana <tatsqui@gmail.com>
2019-03-21 13:26:22 -0700

    commented out tests for superclass Recipient

--- specs/recipient_spec.rb
+++ specs/recipient_spec.rb
@@ -1,29 +1,19 @@
 require_relative "test_helper"
 
-describe "Recipient" do
-  describe "initialize" do
-  end
+# probably don't need any tests for abstract superclass.
+# describe "Recipient" do
+#   describe "initialize" do
+#   end
 
-  describe "send message" do
-    it "can post a message on the everyone slack channel" do
-      VCR.use_cassette("message_post") do
-        name = "everyone"
-        text = "hello world"
-        pigeon = SlackCli::Recipient.send_message(name: name, message: text)
+#   describe "send message" do
+#     it "can post a message on the everyone slack channel" do
+#       VCR.use_cassette("message_post") do
+#         name = "everyone"
+#         text = "hello world"
+#         pigeon = SlackCli::Recipient.send_message(name: name, message: text)
 
-        expect(pigeon["ok"]).must_equal true
-      end
-    end
-
-    it "raises a SlackError with bad parameters" do
-      VCR.use_cassette("message_post") do
-        name = ""
-        text = "this should not send"
-
-        expect {
-          SlackCli::Recipient.send_message(name: name, message: text)
-        }.must_raise SlackCli::SlackError
-      end
-    end
-  end
-end
+#         expect(pigeon["ok"]).must_equal true
+#       end
+#     end
+#   end
+# end
