d5493b26f5ea7093cd388c5c21a0d2881f21cab1
MyriamWD <myriam.waldend@gmail.com>
2019-03-21 15:31:51 -0700

    created methods to handle the disoplay of options and information

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,43 @@
 #!/usr/bin/env ruby
+require_relative "workspace"
+require_relative "user"
 
-def main
-  puts "Welcome to the Ada Slack CLI!"
+module SlackCLI
+  class Main
+    def self.main
+      puts "Welcome to the Ada Slack CLI!"
+      option = 0
+      until option == 3
+        puts "---Options---"
+        puts "  1. List Users"
+        puts "  2. List Channels"
+        puts "  3. Quit"
+        puts "\n\nPlease, choose one of the options available"
+        option = gets.chomp.to_i
+        self.options(option)
+      end
+      puts "Thank you for using the Ada Slack CLI"
+      exit
+    end
 
-  # TODO: project
-
-  puts "Thank you for using the Ada Slack CLI"
+    def self.options(option)
+      if option != 3
+        if option == 1
+          workspace = Workspace.new
+          users_list = workspace.users_list
+          puts "----List of Slack Users----\n\n"
+          users_list.each_with_index do |user, index|
+            puts "User N.#{index + 1}"
+            puts "  Name: #{user.name}"
+            puts "  Real Name:#{user.real_name}"
+            puts "  ID: #{user.id}\n\n"
+          end
+          puts "Press any key to continue...\n\n"
+          gets.chomp
+        end
+      end
+    end
+  end
 end
 
-main if $PROGRAM_NAME == __FILE__
+SlackCLI::Main.main if $PROGRAM_NAME == __FILE__
