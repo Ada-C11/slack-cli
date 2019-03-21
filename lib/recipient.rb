e3c6412e09713ddc4144ceb037e3f5677410062e
Angela <angela.ohh@gmail.com>
2019-03-21 13:36:03 -0700

    fixed our ability to send messages to user and aligned with project requirements

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,6 +1,8 @@
 require "httparty"
 require "dotenv"
 require_relative "workspace"
+require_relative "user"
+require_relative "channel"
 
 Dotenv.load
 
@@ -11,6 +13,13 @@ module SlackApi
   class SlackApiError < StandardError; end
 
   def self.send_message(message, user)
+    user_name = User.list.map do |user|
+      user[0]
+    end
+    # array of all channel names
+    channel_name = Channel.list.map do |channel|
+      channel[0]
+    end
     #   user_id = User.list.each do |id|
     #     if user == id[0]
     #       user[2]
@@ -21,16 +30,29 @@ module SlackApi
     # else if user input is a type of channel choose 2nd response. the channel can be select_user or select_channel from the main
     # we'd have to require workspace
     test_workspace = Workspace.new
-    response = HTTParty.post(
-      "#{BASE_URL}",
-      headers: { "Content-Type" => "application/x-www-form-urlencoded" },
-      body: {
-        token: SLACK_TOKEN,
-        text: message,
-        channel: test_workspace.select_channel(user), #.select_user(chose_user)
-        as_user: true,
-      },
-    )
+    if channel_name.include?(user)
+      response = HTTParty.post(
+        "#{BASE_URL}",
+        headers: { "Content-Type" => "application/x-www-form-urlencoded" },
+        body: {
+          token: SLACK_TOKEN,
+          text: message,
+          channel: test_workspace.select_channel(user), #.select_user(chose_user)
+          as_user: true,
+        },
+      )
+    elsif user_name.include?(user)
+      response = HTTParty.post(
+        "#{BASE_URL}",
+        headers: { "Content-Type" => "application/x-www-form-urlencoded" },
+        body: {
+          token: SLACK_TOKEN,
+          text: message,
+          channel: test_workspace.select_user(user), #.select_user(chose_user)
+          as_user: true,
+        },
+      )
+    end
 
     if response["ok"]
       return true
