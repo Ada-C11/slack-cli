1a18c7dded5ca42777bcda236938d7bff7204507
Tatiana Quintana <tatsqui@gmail.com>
2019-03-21 14:05:37 -0700

    added more selected_user/channel validation tests

--- specs/workspace_spec.rb
+++ specs/workspace_spec.rb
@@ -1,6 +1,6 @@
-require_relative 'test_helper'
+require_relative "test_helper"
 
-describe "Workspace class" do 
+describe "Workspace class" do
   before(:each) do
     VCR.insert_cassette("workspace_response")
   end
@@ -12,43 +12,46 @@ describe "Workspace class" do
   }
 
   describe "initialize" do
-    it "returns an array of User instances" do 
+    it "returns an array of User instances" do
       expect(workspace.users).must_be_kind_of Array
-      expect(workspace.users.first).must_be_kind_of SlackCli::User
+      expect(workspace.users.first).must_be_instance_of SlackCli::User
+      expect(workspace.users.last).must_be_instance_of SlackCli::User
     end
 
-    it "returns an array of Channel instances" do 
-      expect(workspace.channels).must_be_kind_of Array 
-      expect(workspace.channels.first).must_be_kind_of SlackCli::Channel
+    it "returns an array of Channel instances" do
+      expect(workspace.channels).must_be_kind_of Array
+      expect(workspace.channels.first).must_be_instance_of SlackCli::Channel
+      expect(workspace.channels.last).must_be_instance_of SlackCli::Channel
     end
 
-    it "returns nil for no selected class" do
-      expect(workspace.selected).must_equal nil
-    #   expect {
-    #     workspace.selected
-    #   }.must_raise SlackCli::SlackError
-    end      
+    it "@selected is nil prior to selecting a user or channel" do 
+      assert_nil(workspace.selected)
+    end
   end
 
-  describe "select_channel method" do 
-    it "returns selected instance of channel" do 
+  describe "select_channel method" do
+    it "returns selected instance of channel" do
       expect(workspace.select_channel("everyone")).must_be_instance_of SlackCli::Channel
       expect(workspace.select_channel("everyone").name).must_equal "everyone"
+      expect(workspace.selected.name).must_equal "everyone"
+      expect(workspace.selected.slack_id).must_equal "CH2RAKATW"
     end
 
-    it "returns nil if no channel selected" do 
+    it "returns nil if no channel selected" do
       expect(workspace.select_channel("")).must_equal nil
     end
   end
 
-  describe "select_user method" do 
-    it "returns selected instance of user" do 
+  describe "select_user method" do
+    it "returns selected instance of user" do
       expect(workspace.select_user("hanalways")).must_be_instance_of SlackCli::User
       expect(workspace.select_user("tatsqui").name).must_equal "tatsqui"
+      expect(workspace.selected.name).must_equal "tatsqui"
+      expect(workspace.selected.slack_id).must_equal "UH2NZ7X7V"
     end
 
-    it "returns nil if no user selected" do 
-      expect(workspace.select_user("")).must_equal nil
+    it "returns nil if no user selected" do
+      assert_nil(workspace.select_user(""))
     end
   end
 end
