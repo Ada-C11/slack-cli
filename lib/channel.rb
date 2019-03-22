eef74a45ca3685d8499649cd3c6e4a30a38f32e2
Bitaman <b.amani.s@gmail.com>
2019-03-22 10:21:04 -0700

    trying to merge

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,43 +1,5 @@
 require_relative "recipient"
 
-<<<<<<< HEAD
-# class Channel < Recipient
-#   BASE_URL = "https://slack.com/api/channels.list"
-#   TOKEN = ENV["SLACK_TOKEN"]
-
-#   class SlackAPIError < StandardError; end
-
-#   def initialize
-#     super
-#     @topic = topic
-#     @member_count = member_count
-#   end
-
-#   #   def details
-#   #   end
-
-#   def self.list
-#     query_params = {
-#       token: TOKEN,
-#     }
-#     response = HTTParty.get(BASE_URL, query: query_params)
-#     # responses = response["response"]
-
-#     # if response.code != 200 || response == nil
-#     #   raise SlackAPIError
-#     # end
-
-#     # responses.each do |pass|
-#     #   puts "#{pass["topic"].to_s}"
-#     #   puts "#{pass["members"].to_a}"
-#     # end
-#   end
-# end
-
-# puts Channel.list
-
-# , "API call failed with code #{response.code} and reason '#{response["reason"]}"
-=======
 class Channel < Recipient
   attr_reader :topic, :member_count
 
@@ -72,4 +34,3 @@ class Channel < Recipient
     return channel_list
   end
 end
->>>>>>> 8f059c8f71fe014a17318c0ca080ae80445fd273
