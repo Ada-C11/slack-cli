6733aa60d702e0eb003afc10f1ba20b29e827684
amyesh <amyesh08@gmail.com>
2019-03-22 10:09:10 -0700

    added table_print functionality to Slack CLI

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,37 +1,43 @@
 require_relative "recipient"
 require_relative "user"
 require_relative "channel"
+require_relative "workspace"
 require "dotenv"
 require "httparty"
+require "table_print"
 Dotenv.load
-require "ap"
 
 def main
+  slack = Workspace.new
   puts "Welcome to the Ada Slack CLI!"
   puts "Choose from the following options:\n1. List users\n2. List Channels\n3. Select User\n4. Select Channel\n5. Details\n6. Quit"
   choice = gets.chomp.upcase
+
   until choice == "QUIT"
     if choice == "LIST USERS"
       puts "\n"
-      puts User.list
+      tp slack.users
+      puts "\n"
     elsif choice == "LIST CHANNELS"
       puts "\n"
-      puts Channel.list
+      tp slack.channels
+      puts "\n"
     elsif choice == "SELECT USER"
       puts "Please enter a username or Slack ID:"
-      user_input = gets.chomp.upcase
+      user_input = gets.chomp
+      slack.select_user(user_input)
     elsif choice == "SELECT CHANNEL"
       puts "Please enter channel name or Slack ID:"
-      channel_input = gets.chomp.upcase
+      channel_input = gets.chomp
+      slack.select_channel(channel_input)
     elsif choice == "DETAILS"
-      #call workspace.selected
-      #if that's nil or empty, raise error
-      #else call details method on selected
-      #return details
+      slack.show_details
     end
+
     puts "Choose from the following options:\n1. List users\n2. List Channels\n3. Select User\n4. Select Channel\n5. Details\n6. Quit"
     choice = gets.chomp.upcase
   end
+
   puts "Thank you for using the Ada Slack CLI"
 end
 
