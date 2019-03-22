39859fdf4cf73de5bfd1db9e28dcf77a72e42537
Elise Pham <elise.pham88@gmail.com>
2019-03-22 10:56:15 -0700

    working on CLI

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,9 +1,49 @@
 #!/usr/bin/env ruby
+require_relative "workspace"
 
 def main
-  puts "Welcome to the Ada Slack CLI!"
+  workspace = Slack::Workspace.new
+  puts "\nWelcome to the Ada Slack CLI!"
+  puts "-------------------------------"
+  puts "\nWorkspace: Sav-Elise-API Project"
+  puts "Number of users: #{workspace.users.length}"
+  puts "Number of channels: #{workspace.channels.length}\n\n"
+  options = ["List Users", "List Channels", "Select User", "Select Channel", "Details", "Quit"]
 
-  # TODO project
+  while true
+    puts "--------OPTIONS--------"
+    puts options
+    print "\nPlease enter your command: "
+    input = gets.chomp.upcase
+
+    case input
+    when "LIST USERS"
+      list = workspace.list_all(list: workspace.users)
+      if list.empty?
+        puts "Unable to access users at this time."
+      else
+        puts "\n" + list + "\n"
+      end
+    when "LIST CHANNELS"
+      list = workspace.list_all(list: workspace.channels)
+      if list.empty?
+        puts "Unable to access channels at this time."
+      else
+        puts "\n" + list + "\n"
+      end
+    when "SELECT USER"
+      print "Please enter user's name, real name or id: "
+      input = gets.chomp.upcase
+      if workspace.find_user(input: input)
+      end
+    when "SELECT CHANNEL"
+    when "DETAILS"
+    when "QUIT"
+      break
+    else
+      puts "Command not recognized. Try again!\n\n"
+    end
+  end
 
   puts "Thank you for using the Ada Slack CLI"
 end
