de4fa527070e27276c513a422d8b754df8e1753a
Tatiana Quintana <tatsqui@gmail.com>
2019-03-22 11:09:05 -0700

    added missing end workspace.rb

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -41,7 +41,8 @@ class Workspace
       if message.length == 0
         raise SlackCli::SlackError, "Message must cannot be nil or blank..."
       else
-      @selected.post_message(@selected.slack_id, message)
+        @selected.post_message(@selected.slack_id, message)
+      end
     end
   end
 
